4f45dac6dc505163da6f3b7457f070da25c5e4a6
unknown <josephgenescar@gmail.com>
2026-04-08 14:52:14 -0400

    m

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Konektem Service Worker v4.0 — PWA Offline konplè
-var CACHE_NAME = 'konektem-v1775673272';
+var CACHE_NAME = 'konektem-v1775673969';
 var STATIC_URLS = [
   '/',
   '/index.html',
